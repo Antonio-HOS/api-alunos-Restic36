8b2d9e338ab2ebe9f55574dc169841c36730e561
Joabe Andrade do Nascimento <joabeandrade2k01@gmail.com>
2024-11-03 18:30:48 -0300

    Criando a rota post

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const {create,update} = require('./repositories/alunosRepository');
+const express = require("express");
+const { create } = require("./repositories/alunosRepository");
 
 const app = express();
 const port = 3000;
+
+app.post("/alunos", (req, res) => {
+  const { nome, email, nome_curso } = req.body;
+  const aluno = create({ nome, email, nome_curso });
+  res.status(201).json(aluno);
+});
 
 app.use(express.json());
