667a4d786ba23ca4b33aacbe6d930a3511e64a5b
Joabe Andrade do Nascimento <joabeandrade2k01@gmail.com>
2024-11-03 18:33:47 -0300

    Fazendo a verificação de email na rota post

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const port = 3000;
 
 app.post("/alunos", (req, res) => {
   const { nome, email, nome_curso } = req.body;
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: "E-mail inválido" });
+  }
+
   const aluno = create({ nome, email, nome_curso });
   res.status(201).json(aluno);
 });
